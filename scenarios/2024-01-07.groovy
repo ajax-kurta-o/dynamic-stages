e05a3f95ae56dd279bb2b6accff5e7d7ab7f5926
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-07 14:14:04 +0200

    Add TESTJenkinsfile.groovy

--- scenarios/2024-01-07.groovy
+++ scenarios/2024-01-07.groovy
@@ -4,18 +4,22 @@ def getStages() {
 
     return [
         [
-            name: "Deploy a911-svc 1.118.0-7447.RELEASE",
+            name: "Deploy services"
+
             steps: {
-                script {
-                    echo "In deploy a911-svc"
-                }
-            }
-        ],
-        [
-            name: "Deploy mobile-gw-svc 1.29.0-8984.RELEASE",
-            steps: {
-                script {
-                    echo "In deploy mobile-gw-svc"
+                parallel: {
+                    name: "Deploy a911-svc 1.118.0-7447.RELEASE",
+                    steps: {
+                        script {
+                            echo "In deploy a911-svc"
+                        }
+                    },
+                    name: "Deploy mobile-gw-svc 1.29.0-8984.RELEASE",
+                    steps: {
+                        script {
+                            echo "In deploy mobile-gw-svc"
+                        }
+                    }
                 }
             }
         ],

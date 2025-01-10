d1caa314b135d4548cef2b74b0c1e9926b710450
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-10 03:05:55 +0200

    Change test groovy script

--- scenarios/2024-01-07.groovy
+++ scenarios/2024-01-07.groovy
@@ -2,14 +2,16 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 def performStages() {
     return {
-        parallel (
-            'Deploy A': {
-                    sh "echo 'This is stage a'"
-            }
-            'Deploy B': {
-                    sh "echo 'This is stage b'"
-            }
-        )
+        stage("a") {
+            parallel (
+                'Deploy A': {
+                        sh "echo 'This is stage a'"
+                }
+                'Deploy B': {
+                        sh "echo 'This is stage b'"
+                }
+            )
+        }
     }
 }
 

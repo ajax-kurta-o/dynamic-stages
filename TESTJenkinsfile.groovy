2edb3a8d5b4b39e1874f097aa0e0f1ebf50a8737
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-10 04:27:28 +0200

    Change test groovy script

--- TESTJenkinsfile.groovy
+++ TESTJenkinsfile.groovy
@@ -12,7 +12,38 @@ pipeline {
             steps {
                 script {
                     if (rollout_stage_passed) {
-                        parallelCall()
+                        stage('Parallel Stage') {
+        failFast true
+        parallel {
+            stage('Branch A') {
+                agent any
+                steps {
+                    echo "On Branch A"
+                }
+            }
+            stage('Branch B') {
+                agent any
+                steps {
+                    echo "On Branch B"
+                }
+            }
+            stage('Branch C') {
+                agent any
+                stages {
+                    stage('Nested 1') {
+                        steps {
+                            echo "In stage Nested 1 within Branch C"
+                        }
+                    }
+                    stage('Nested 2') {
+                        steps {
+                            echo "In stage Nested 2 within Branch C"
+                        }
+                    }
+                }
+            }
+        }
+    }
                     } else {
 
                     }

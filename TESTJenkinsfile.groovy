a84b54500008a2349732e1c2ae4748eb70cc4916
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-10 04:26:37 +0200

    Change test groovy script

--- TESTJenkinsfile.groovy
+++ TESTJenkinsfile.groovy
@@ -11,7 +11,54 @@ pipeline {
         stage("DYNAMIC_STAGES") {
             steps {
                 script {
-                    load '/var/jenkins_home/workspace/TestPipeline2_dynamic-stages/scenarios/2024-01-07.groovy'
+                    if (rollout_stage_passed) {
+                        parallelCall()
+                    } else {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+def sequentialCall() {
+    stage('Non-Parallel Stage') {
+        steps {
+            echo 'This stage will be executed first.'
+        }
+    }
+}
+
+def parallelCall() {
+    stage('Parallel Stage') {
+        failFast true
+        parallel {
+            stage('Branch A') {
+                agent any
+                steps {
+                    echo "On Branch A"
+                }
+            }
+            stage('Branch B') {
+                agent any
+                steps {
+                    echo "On Branch B"
+                }
+            }
+            stage('Branch C') {
+                agent any
+                stages {
+                    stage('Nested 1') {
+                        steps {
+                            echo "In stage Nested 1 within Branch C"
+                        }
+                    }
+                    stage('Nested 2') {
+                        steps {
+                            echo "In stage Nested 2 within Branch C"
+                        }
+                    }
                 }
             }
         }

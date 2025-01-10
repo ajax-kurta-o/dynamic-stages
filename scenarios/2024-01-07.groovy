72e63b09d7e9baa5df03875651cfb708fbc6a394
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-10 02:50:59 +0200

    Change test groovy script

--- scenarios/2024-01-07.groovy
+++ scenarios/2024-01-07.groovy
@@ -1,52 +1,42 @@
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 def performStages() {
-    def parallelStages1 = [
-        [
-            name: "Deploy services",
-            stages: [
-                [stage_name: "deploy_a", steps: [ { -> sh "echo 'This is stage a'" } ]],
-                [stage_name: "deploy_b", steps: [ { -> sh "echo 'This is stage b'" } ]]
-            ]
-        ]
-    ]
-
-    def run1 = [
-        [
-            name: "Run BDD tests",
-            stages: [
-                [stage_name: "Run tests", steps: [ { -> sh "echo 'Run tests'" } ]]
-            ]
-        ]
-    ]
-
     return {
-        parallel parallelStages1.collectEntries { dynamicStage ->
-            [(dynamicStage.name): {
-                dynamicStage.stages.each { stage ->
-                    stage(stage.stage_name) {
-                        stage.steps.each { step ->
-                            step.call()
+        // Parallel stages for "Deploy services"
+        stage("Deploy services") {
+            script {
+                parallel(
+                    deploy_a: {
+                        stage("deploy_a") {
+                            steps {
+                                sh "echo 'This is stage a'"
+                            }
+                        }
+                    },
+                    deploy_b: {
+                        stage("deploy_b") {
+                            steps {
+                                sh "echo 'This is stage b'"
+                            }
                         }
                     }
-                }
-            }]
+                )
+            }
         }
 
-        // Sequentially run tests after all parallel stages
-        run1.each { sequentialStage ->
-            stage(sequentialStage.name) {
-                sequentialStage.stages.each { stage ->
-                    stage(stage.stage_name) {
-                        stage.steps.each { step ->
-                            step.call()
-                        }
-                    }
+        // Sequential stage for "Run BDD tests"
+        stage("Run BDD tests") {
+            stage("Run tests") {
+                steps {
+                    sh "echo 'Run tests'"
                 }
             }
         }
     }
 }
+
+return this
+
 
 
 return this

b94eb90c4ed17f7410988ced2dc1dc70bad33be8
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-10 02:29:43 +0200

    Change test groovy script

--- scenarios/2024-01-07.groovy
+++ scenarios/2024-01-07.groovy
@@ -4,34 +4,35 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 
 def performStages() {
-    parallel_stages_1 = [
+    def parallelStages1 = [
         [
             name: "Deploy services",
             stages: [
-                    [stage_name: "deploy_a", steps: [ {"echo 'This is stage a'"} ]],
-                    [stage_name: "deploy_b", steps: [ {"echo 'This is stage b'"} ]],
-                ]
+                [stage_name: "deploy_a", steps: [ { -> sh "echo 'This is stage a'" } ]],
+                [stage_name: "deploy_b", steps: [ { -> sh "echo 'This is stage b'" } ]]
+            ]
         ]
     ]
 
-    run_1 = [
+    def run1 = [
         [
-            name: "Run BDD tests"
-            stages: [stage_name: "Run tests", steps: [ {echo 'Run tests'} ]],
-         ]
+            name: "Run BDD tests",
+            stages: [
+                [stage_name: "Run tests", steps: [ { -> sh "echo 'Run tests'" } ]]
+            ]
+        ]
     ]
+
     return {
-        parallel_stages_1.each { dynamicStage ->
+        parallelStages1.each { dynamicStage ->
             stage(dynamicStage.name) {
                 script {
                     parallel dynamicStage.stages.collectEntries { stage ->
-                        stage (stage.stage_name) {
-                            script {
-                                stage.steps.each { step ->
-                                    step.call()
-                                }
+                        [(stage.stage_name): {
+                            stage.steps.each { step ->
+                                step.call()
                             }
-                        }
+                        }]
                     }
                 }
             }

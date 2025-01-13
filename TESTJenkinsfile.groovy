169cfc4bed8e5dd390bf18e03e541355bdf60b49
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-13 13:17:55 +0200

    Change test groovy script

--- TESTJenkinsfile.groovy
+++ TESTJenkinsfile.groovy
@@ -1,45 +1,38 @@
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 pipeline {
     agent any
     stages {
         stage('ROLLOUT') {
-                steps {
-                    sh "echo Rollout!"
-                    script{ rollout_stage_passed = true }
-                }
+            steps {
+                sh "echo Rollout!"
+                script { rollout_stage_passed = true }
+            }
         }
         stage("DYNAMIC_STAGES") {
             steps {
                 script {
-                    if (rollout_stage_passed == true) {
-
-                    }
-                    else {
+                    if (rollout_stage_passed) {
+                        def dynamicLib = getDynamicStages()
+                        def performStages = dynamicLib.performStages()
+                        performStages.call() // Invoke the closure
+                    } else {
                         echo "Skip running dynamic stages due to failed rollout process"
                     }
                 }
             }
         }
-
     }
 }
 
-
-
 def getDynamicStages() {
     try {
-        dynamicStagesFile = '/Users/oleksandrkurta/WORK/own-dynamic-stages/scenarios/2024-12-07.groovy'
-        sh 'echo in_dynamic'
+        def dynamicStagesFile = '/var/jenkins_home/workspace/TestPipeline2_dynamic-stages/scenarios/2024-01-07.groovy'
         if (fileExists(dynamicStagesFile)) {
-            sh 'echo file exist'
             return load(dynamicStagesFile)
         }
-        return []
-    }
-    catch (Exception exc) {
+        return null
+    } catch (Exception exc) {
         currentBuild.result = "UNSTABLE"
         unstable("Failed loading dynamic stages from remote repository. Error: ${exc}")
-
-        return []
+        return null
     }
 }
--- scenarios/2024-01-07.groovy
+++ scenarios/2024-01-07.groovy
@@ -1,93 +1,36 @@
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-
-// def performStages() {
-//     def parallelStages1 = [
-//         [
-//             name: "Deploy services",
-//             stages: [
-//                 [stage_name: "deploy_a", steps: [ { -> sh "echo 'This is stage a'" } ]],
-//                 [stage_name: "deploy_b", steps: [ { -> sh "echo 'This is stage b'" } ]]
-//             ]
-//         ]
-//     ]
-//
-//     def run1 = [
-//         [
-//             name: "Run BDD tests",
-//             stages: [
-//                 [stage_name: "Run tests", steps: [ { -> sh "echo 'Run tests'" } ]]
-//             ]
-//         ]
-//     ]
-//
-//     return {
-//         parallelStages1.each { dynamicStage ->
-//             stage(dynamicStage.name) {
-//                 script {
-//                     parallel dynamicStage.stages.collectEntries { stage ->
-//                         [(stage.stage_name): {
-//                             stage.steps.each { step ->
-//                                 step.call()
-//                             }
-//                         }]
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-def number() {
-    return 2
-}
-
-def deployNumber() {
-    return [2, 5]
-}
-
-
 def performStages() {
+    def parallelStages1 = [
+        [
+            name: "Deploy services",
+            stages: [
+                [stage_name: "deploy_a", steps: [ { -> sh "echo 'This is stage a'" } ]],
+                [stage_name: "deploy_b", steps: [ { -> sh "echo 'This is stage b'" } ]]
+            ]
+        ]
+    ]
 
-    return [
-        [
-            name: "Deploy a911-svc 1.118.0-7447.RELEASE",
-            steps: {
-                sh 'echo This is deploy a911'
-            }
-        ],
-        [
-            name: "Deploy mobile-svc 1.118.0-7447.RELEASE",
-            steps: {
-                sh 'echo This is deploy mobile'
-            }
-        ],
+    def run1 = [
         [
             name: "Run BDD tests",
-            steps: {
-                sh 'echo This is tests'
-            }
-        ],
-
-            name: "Deploy a911-svc2 1.118.0-7447.RELEASE",
-            steps: {
-                sh 'echo This is deploy a9112'
-            }
-        ],
-        [
-            name: "Deploy mobile-svc2 1.118.0-7447.RELEASE",
-            steps: {
-                sh 'echo This is deploy mobile2'
-            }
-        ],
-        [
-            name: "Run BDD tests2",
-            steps: {
-                sh 'echo This is tests2'
-            }
-        ],
+            stages: [
+                [stage_name: "Run tests", steps: [ { -> sh "echo 'Run tests'" } ]]
+            ]
+        ]
     ]
+
+    return {
+        parallel parallelStages1.collectEntries { dynamicStage ->
+            [(dynamicStage.name): {
+                dynamicStage.stages.each { stage ->
+                    stage(stage.stage_name) {
+                        stage.steps.each { step ->
+                            step.call()
+                        }
+                    }
+                }
+            }]
+        }
+    }
 }
 
 return this
-
-

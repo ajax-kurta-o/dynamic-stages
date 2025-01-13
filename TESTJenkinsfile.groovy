430c9301c5b8912202e3827a1a88e0f2ba0aad0f
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-13 13:30:42 +0200

    Change test groovy script

--- TESTJenkinsfile.groovy
+++ TESTJenkinsfile.groovy
@@ -12,7 +12,8 @@ pipeline {
                 script {
                     if (rollout_stage_passed) {
                         def dynamicLib = getDynamicStages()
-                        dynamicLib.performStages().call()
+                        def performStages = dynamicLib.performStages()
+                        performStages.call() // Invoke the closure
                     } else {
                         echo "Skip running dynamic stages due to failed rollout process"
                     }
@@ -24,7 +25,7 @@ pipeline {
 
 def getDynamicStages() {
     try {
-        def dynamicStagesFile = '/Users/oleksandrkurta/WORK/own-dynamic-stages/scenarios/first_example.groovy'
+        def dynamicStagesFile = '/var/jenkins_home/workspace/TestPipeline2_dynamic-stages/scenarios/first_example.groovy'
         if (fileExists(dynamicStagesFile)) {
             return load(dynamicStagesFile)
         }

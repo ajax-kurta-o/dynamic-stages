73e8e59aa175ea51e92dd6005c77993a6b1bef66
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-13 14:32:18 +0200

    Change test groovy script

--- TESTINFILEJenkinsfile.groovy
+++ TESTINFILEJenkinsfile.groovy
@@ -32,31 +32,33 @@ pipeline {
                 }
             }
         }
-        parallel {
-            stage('Branch A') {
-                agent any
-                steps {
-                    echo "On Branch A"
-                }
-            }
-            stage('Branch B') {
-                agent any
-                steps {
-                    echo "On Branch B"
-                }
-            }
-            stage('Branch C') {
-                agent any
-
-                stages {
-                    stage('Nested 1') {
-                        steps {
-                            echo "In stage Nested 1 within Branch C"
-                        }
+        stage ('Parallel') {
+            parallel {
+                stage('Branch A') {
+                    agent any
+                    steps {
+                        echo "On Branch A"
                     }
-                    stage('Nested 2') {
-                        steps {
-                            echo "In stage Nested 2 within Branch C"
+                }
+                stage('Branch B') {
+                    agent any
+                    steps {
+                        echo "On Branch B"
+                    }
+                }
+                stage('Branch C') {
+                    agent any
+
+                    stages {
+                        stage('Nested 1') {
+                            steps {
+                                echo "In stage Nested 1 within Branch C"
+                            }
+                        }
+                        stage('Nested 2') {
+                            steps {
+                                echo "In stage Nested 2 within Branch C"
+                            }
                         }
                     }
                 }

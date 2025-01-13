bf211acbdb467d523092c054bc3dfa0222e14cca
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-13 10:58:23 +0200

    Change test groovy script

--- scenarios/2024-12-07.groovy
+++ scenarios/2024-12-07.groovy
@@ -1,116 +1,57 @@
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+pipeline {
+    agent any
+    stages {
+            stage('Parallel Deployments') {
+                parallel {
+                    stage("Deploy external-device-svc 1.36.1-306.RELEASE") {
+                        steps{
+                            script {
+                                echo "Deploy external-device-svc 1.36.1-306.RELEASE"
+                            }
+                        }
+                    }
 
-def getStages() {
-    rc_testing = load("rollout_stage/jenkins/rc_testing.groovy")
+                    stage("Deploy mobile-svc 1.36.1-306.RELEASE") {
+                        steps{
+                            script {
+                                echo "Deploy mobile-svc 1.36.1-306.RELEASE"
+                            }
+                        }
+                    }
+                }
+            }
+            stage("Run BDD tests with marks: marks") {
+                steps{
+                    script {
+                        echo "run tests"
+                    }
+                }
+            }
+            stage('Parallel Deployments') {
+                parallel {
+                      stage("Deploy a911-device-svc 1.36.1*.RELEASE") {
+                          steps{
+                              script {
+                                  echo "Deploy a911-device-svc 1.36.1*.RELEASE"
+                              }
+                          }
+                      }
 
-    def getDynamicStagesResults = {
-        if (env.dynamicStagesResults) {
-            return readJSON(text: env.dynamicStagesResults)
-        }
-        return [:]
+                      stage("Deploy desktop-device-svc 1.36.1*.RELEASE") {
+                          steps{
+                              script {
+                                  echo "Deploy desktop-device-svc 1.36.1*.RELEASE"
+                              }
+                          }
+                      }
+                }
+            }
+            stage("Run BDD tests with marks: marks") {
+                steps{
+                    script {
+                       echo "Run BDD tests with marks: marks"
+                    }
+                }
+            }
     }
-
-    def saveFailedDeploy = {service, version ->
-        def failedRCDeploys = env.failedRCDeploys ? env.failedRCDeploys.split(',').toList() : []
-
-        failedRCDeploys.add("Deploy ${service} ${version}")
-        env.failedRCDeploys = failedRCDeploys.join(",")
-    }
-
-    return [
-        [
-            name: "Deploy external-device-svc 1.36.1-306.RELEASE",
-            steps: {
-                script {
-                    dynamicStagesResults = getDynamicStagesResults()
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        def serviceVersionFromPattern = rc_testing.getLatestServiceVersionByPattern(
-                            serviceName="external-device-svc",
-                            serviceVersionPattern="1.36.1-306.RELEASE"
-                        )
-
-                        dynamicStagesResults['deploy_external_device_svc_passed'] = rc_testing.deployService(
-                            serviceName="external-device-svc",
-                            serviceVersion=serviceVersionFromPattern
-                        )
-                        if (dynamicStagesResults['deploy_external_device_svc_passed'] == false) {
-                          saveFailedDeploy("external-device-svc", "1.36.1-306.RELEASE")
-                        }
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key
-                        echo "Skip deploy due to failure: ${failedStage} == false"
-                        Utils.markStageSkippedForConditional(env.STAGE_NAME)
-                    }
-
-                    env.dynamicStagesResults = groovy.json.JsonOutput.toJson(dynamicStagesResults)
-                }
-            }
-        ],
-        [
-            name: "Run BDD tests with marks: regress",
-            steps: {
-                script {
-                    dynamicStagesResults = getDynamicStagesResults()
-
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        rc_testing.clearDatabases()
-                        rc_testing.respawnActors()
-                        dynamicStagesResults['setup_generation_passed'] = rc_testing.generateSetups()
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key
-                        echo "Skip setup generation due to failure: ${failedStage} == false"
-                    }
-
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        rc_testing.runBDDTests(
-                            marks='regress',
-                            test_plan_name='RC [external-device-svc 1.36.1-306.RELEASE]'
-                        )
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key 
-                        echo "Skip running BDD tests due to failure: ${failedStage} == false"
-                        if (failedStage != "setup_generation_passed") {
-                            Utils.markStageSkippedForConditional(env.STAGE_NAME)
-                        }
-                    }
-
-                    env.dynamicStagesResults = ""
-                }
-            }
-        ],
-        [
-            name: "Deploy external-device-svc 1.36.1*.RELEASE",
-            steps: {
-                script {
-                    dynamicStagesResults = getDynamicStagesResults()
-                    if (dynamicStagesResults.every { stage_passed -> stage_passed.value == true }) {
-                        def serviceVersionFromPattern = rc_testing.getLatestServiceVersionByPattern(
-                            serviceName="external-device-svc",
-                            serviceVersionPattern="1.36.1*.RELEASE"
-                        )
-
-                        dynamicStagesResults['deploy_external_device_svc_passed'] = rc_testing.deployService(
-                            serviceName="external-device-svc",
-                            serviceVersion=serviceVersionFromPattern
-                        )
-                        if (dynamicStagesResults['deploy_external_device_svc_passed'] == false) {
-                          saveFailedDeploy("external-device-svc", "1.36.1*.RELEASE")
-                        }
-                    }
-                    else {
-                        def failedStage = dynamicStagesResults.find { stage_passed -> stage_passed.value == false }?.key
-                        echo "Skip deploy due to failure: ${failedStage} == false"
-                        Utils.markStageSkippedForConditional(env.STAGE_NAME)
-                    }
-
-                    env.dynamicStagesResults = groovy.json.JsonOutput.toJson(dynamicStagesResults)
-                }
-            }
-        ],
-    ]
 }
-
-return this

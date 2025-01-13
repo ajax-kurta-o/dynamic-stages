2d760f4758b91c0a7d09e6dbfc88301b647cf054
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-13 11:22:29 +0200

    Change test groovy script

--- scenarios/2024-12-07.groovy
+++ scenarios/2024-12-07.groovy
@@ -2,56 +2,57 @@ pipeline {
     agent any
     stages {
             stage('Parallel Deployments1') {
-                parallel {
-                    stage("Deploy external-device-svc 1.36.1-306.RELEASE") {
-                        steps{
-                            script {
-                                echo "Deploy external-device-svc 1.36.1-306.RELEASE"
-                            }
-                        }
-                    }
+            fastFail: True
+            parallel {
+                       stage("Deploy external-device-svc 1.36.1-306.RELEASE") {
+            steps{
+                script {
+                    echo "Deploy external-device-svc 1.36.1-306.RELEASE"
 
-                    stage("Deploy mobile-svc 1.36.1-306.RELEASE") {
-                        steps{
-                            script {
-                                echo "Deploy mobile-svc 1.36.1-306.RELEASE"
-                            }
-                        }
-                    }
                 }
             }
-            stage("Run BDD tests with marks: marks1") {
-                steps{
-                    script {
-                        echo "run tests"
-                    }
+        }
+        stage("Deploy mobile-svc 1.36.1-306.RELEASE") {
+            steps{
+                script {
+                    echo "Deploy mobile-svc 1.36.1-306.RELEASE"
                 }
             }
-            stage('Parallel Deployments2') {
-                parallel {
-                      stage("Deploy a911-device-svc 1.36.1*.RELEASE") {
-                          steps{
-                              script {
-                                  echo "Deploy a911-device-svc 1.36.1*.RELEASE"
-                              }
-                          }
-                      }
-
-                      stage("Deploy desktop-device-svc 1.36.1*.RELEASE") {
-                          steps{
-                              script {
-                                  echo "Deploy desktop-device-svc 1.36.1*.RELEASE"
-                              }
-                          }
-                      }
+        }
+            }
+        }
+        stage("Run BDD tests with marks: marks1") {
+            steps{
+                script {
+                    echo "Run BDD tests with marks: marks"
                 }
             }
-            stage("Run BDD tests with marks: marks2") {
-                steps{
-                    script {
-                       echo "Run BDD tests with marks: marks"
-                    }
+        }
+    stage('Parallel Deployments2') {
+            fastFail: True
+            parallel {
+                       stage("Deploy a911-device-svc 1.36.1*.RELEASE") {
+            steps{
+                script {
+                    echo "Deploy a911-device-svc 1.36.1*.RELEASE"
                 }
             }
+        }
+        stage("Deploy desktop-device-svc 1.36.1*.RELEASE") {
+            steps{
+                script {
+                    echo "Deploy desktop-device-svc 1.36.1*.RELEASE"
+                }
+            }
+        }
+            }
+        }
+        stage("Run BDD tests with marks: marks2") {
+            steps{
+                script {
+                    echo "Run BDD tests with marks: marks"
+                }
+            }
+        }
     }
 }

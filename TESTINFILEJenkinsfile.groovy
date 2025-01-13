db85b1eaa284c0af8032eaac5b2a4ae71fbc807b
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-13 14:27:42 +0200

    Change test groovy script

--- TESTINFILEJenkinsfile.groovy
+++ TESTINFILEJenkinsfile.groovy
@@ -32,6 +32,40 @@ pipeline {
                 }
             }
         }
+
+        stage("DYNAMIC_STAGES2") {
+        steps{
+            parallel {
+                stage('Branch A') {
+                    agent any
+                    steps {
+                        echo "On Branch A"
+                    }
+                }
+                stage('Branch B') {
+                    agent any
+                    steps {
+                        echo "On Branch B"
+                    }
+                }
+                stage('Branch C') {
+                    agent any
+
+                    stages {
+                        stage('Nested 1') {
+                            steps {
+                                echo "In stage Nested 1 within Branch C"
+                            }
+                        }
+                        stage('Nested 2') {
+                            steps {
+                                echo "In stage Nested 2 within Branch C"
+                            }
+                        }
+                    }
+                }
+            }
+        }}
     }
 }
 

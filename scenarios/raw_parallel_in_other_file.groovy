57d51066a3ffd06cac4c9839dc8f71112cd4b4ef
KurtaOleksandr <34400974+sashakurta@users.noreply.github.com>
2025-01-13 13:47:13 +0200

    Change test groovy script

---
+++ scenarios/raw_parallel_in_other_file.groovy
@@ -0,0 +1,35 @@
+def performStages() {
+    return {parallel {
+                stage('Branch A') {
+                    agent any
+                    steps {
+                        echo "On Branch A"
+                    }
+                }
+                stage('Branch B') {
+                    agent any
+                    steps {
+                        echo "On Branch B"
+                    }
+                }
+                stage('Branch C') {
+                    agent any
+
+                    stages {
+                        stage('Nested 1') {
+                            steps {
+                                echo "In stage Nested 1 within Branch C"
+                            }
+                        }
+                        stage('Nested 2') {
+                            steps {
+                                echo "In stage Nested 2 within Branch C"
+                            }
+                        }
+                    }
+                }
+            }
+         }
+}
+
+return this
